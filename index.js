2eff0eedefa1c499633c224debda5e9962b0c5f9
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-02-01 20:43:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
-  var obj = Object.assign(object, {key: value});
+  var obj = Object.assign({}, object, {key: value});
   return obj;
 }
